fc92c2321fe13525151d82b65a8f05b8996093e2
Kathleen Weisensel <kweisensel@gmail.com>
2017-12-15 13:40:50 -0600

    search box

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchInput, {createFilter} from 'react-search-input';
 
 class App extends Component {
   render() {
@@ -8,9 +9,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Kathy and Jackie's React App</h1>
+          <h1 className="App-title">Cocktail Rescue!</h1>
         </header>
-        <h2> We don't know what we're doing! </h2>
+        <h2> Search for Recipes:</h2>
+        <SearchInput className="search-input" />
+          {/* return ( */}
+            {/* <div className="recipeTitle" key={title.id}>
+              <div className="recipePic" key={pic.id}>
+            </div>
+            </div> */}
+          {/* )  */}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. Or don't. No one can tell anyone else what to do. You have to do what's right for you.
         </p>
