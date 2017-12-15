a8e80b27fcc7989277f6769c08de7383b05b7b7f
Kathleen Weisensel <kweisensel@gmail.com>
2017-12-15 12:29:44 -0600

    logo and text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Kathy and Jackie's React App</h1>
         </header>
+        <h2> We don't know what we're doing! </h2>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Or don't. No one can tell anyone else what to do. You have to do what's right for you.
         </p>
       </div>
     );
